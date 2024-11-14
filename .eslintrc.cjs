0e1b92f7b3c897531cf1cd93294711495650769a
Rsjii <rsji1806@gmail.com>
2024-11-14 09:07:18 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-// module.exports = {
-//   env: { browser: true, es2020: true },
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:react/recommended',
-//     'plugin:react/jsx-runtime',
-//     'plugin:react-hooks/recommended',
-//   ],
-//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-//   settings: { react: { version: '18.2' } },
-//   plugins: ['react-refresh'],
-//   rules: {
-//     'react-refresh/only-export-components': 'warn',
-//   },
-// }
-=======
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
@@ -30,4 +13,4 @@ module.exports = {
     'react-refresh/only-export-components': 'warn',
   },
 }
->>>>>>> 0672bd1fd3f1f7b482650e07aa5b79ec818fc3a7
+
